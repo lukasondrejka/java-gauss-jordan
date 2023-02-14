fac958f641f1c0e8c3f55af7893b0c134a1032af
Lukáš Ondrejka <lukas.ondrejka1@gmail.com>
2023-02-14 15:03:25 +0100

    Add tests

--- src/test/java/gaussjordan/MatrixSolutionTest.java
+++ src/test/java/gaussjordan/MatrixSolutionTest.java
@@ -9,18 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatrixSolutionTest {
 
     /**
-     * Test for solution
-     * @param matrixData matrix data
-     * @param solution expected solution
-     */
-    private void testSolution(double[][] matrixData, Solution solution) {
-        Matrix matrix = new Matrix(matrixData);
-        matrix.eliminate();
-        assertEquals(solution, matrix.getSolution(),
-                "\nInput matrix: \n" + Helper.matrixToString(matrixData));
-    }
-
-    /**
      * Test for no solution
      */
     @Test
@@ -52,6 +40,16 @@ class MatrixSolutionTest {
     }
 
     /**
+     * Test for unknown solution before Gauss-Jordan elimination
+     */
+    @Test
+    void testUnknownSolution() {
+        Matrix matrix = new Matrix(2, 2);
+
+        assertEquals(Solution.UNKNOWN, matrix.getSolution());
+    }
+
+    /**
      * Test solution values
      */
     @Test
@@ -80,4 +78,15 @@ class MatrixSolutionTest {
         }
     }
 
+    /**
+     * Test for solution
+     * @param matrixData matrix data
+     * @param solution expected solution
+     */
+    private void testSolution(double[][] matrixData, Solution solution) {
+        Matrix matrix = new Matrix(matrixData);
+        matrix.eliminate();
+        assertEquals(solution, matrix.getSolution(),
+                "\nInput matrix: \n" + Helper.matrixToString(matrixData));
+    }
 }
--- src/test/java/gaussjordan/MatrixTest.java
+++ src/test/java/gaussjordan/MatrixTest.java
@@ -21,4 +21,26 @@ class MatrixTest {
             new Matrix(data);
         });
     }
+
+    /**
+     * Test for matrix
+     */
+    @Test
+    void testMatrixGet() {
+        double[][] data = {
+                {1, 2, 3},
+                {4, 5, 6},
+        };
+        Matrix matrix = new Matrix(data);
+
+        for (int i = 0; i < data.length; i++)
+            for (int j = 0; j < data[0].length; j++)
+                assertEquals(data[i][j], matrix.get(i, j));
+
+        // Test for out of bounds
+        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(-1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(0, -1));
+        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(data.length, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> matrix.get(0, data[0].length));
+    }
 }
